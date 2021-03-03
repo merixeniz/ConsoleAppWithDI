42406be216a2a3539c24845a4904350acadb6926
Szymon Oles <szyoles@gmail.com>
2021-03-03 20:52:25 +0100

    initial commit

---
+++ ConsoleAppWithDI/ConsoleAppWithDI/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppWithDI
+{
+    public class App
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World from Run method!");
+
+            Console.Read();
+        }
+    }
+}
